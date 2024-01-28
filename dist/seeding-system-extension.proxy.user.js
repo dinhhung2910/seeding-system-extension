35ff340ae7535afbd7c334cf0e28190c613baea5
Dinh Hung <dinhhung2910@gmail.com>
2024-01-28 13:44:11 +0700

    Allow script make XMLHttpRequest to localhost

--- dist/seeding-system-extension.proxy.user.js
+++ dist/seeding-system-extension.proxy.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name Seeding system extension
 // @description Seeding system extension
-// @version 1.0.6
+// @version 1.0.7
 // @author Hung Dinh
 // @homepage https://github.com/dinhhung2910/seeding-system-extension#readme
 // @supportURL https://github.com/dinhhung2910/seeding-system-extension/pulls
 // @match https://tiktok.com/**
 // @match https://www.tiktok.com/**
+// @connect localhost
 // @grant GM_fetch
 // @grant GM_xmlhttpRequest
 // @namespace Script
